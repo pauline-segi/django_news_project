abf822dec11107f765b2b36686230877f078a58d
Pauline Segi <p.segi@outlook.com>
2023-07-22 09:13:13 +1000

    reversed the profile changes

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -3,8 +3,8 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
@@ -13,6 +13,6 @@ class CreateAccountView(CreateView):
     template_name = 'users/createAccount.html'
 
 
-@login_required
-def profile(request):
-    return render(request, 'users/profile.html')
+# @login_required
+# def profile(request):
+#     return render(request, 'users/profile.html')
